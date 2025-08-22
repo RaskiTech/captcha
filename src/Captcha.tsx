89b6fceea14fb24a924ddf4ad48092ab1829e99d
RaskiTech <tsombi100@gmail.com>
2025-08-22 20:15:00 +0300

    Loading...

--- src/Captcha.tsx
+++ src/Captcha.tsx
@@ -24,16 +24,74 @@ function RenderMessagebox(message: string, clearMessage: () => void,
 	);
 }
 
-const checksInitial = [
-	{ label: 'Was long enough', passed: true },
-	{ label: "Wasn't loud enough", passed: false },
-];
+type Message = { label: string; status: 'pass' | 'fail' | 'loading'; }
 
 export default function Captcha() {
-	const [checks, setChecks] = useState<{ label: string; passed: boolean; }[]>([]);
+	const [checks, setChecks] = useState<Message[]>([]);
+	const [minCheckCount, setMinCheckCount] = useState<number>(0)
 
-	const OnRecordingComplete = (audio: Blob) => {
-		setChecks(checksInitial)
+	const IsLoudEnough: (audio: Blob, duration: number) => Promise<Message> = async (audio: Blob, duration: number) => {
+		const Decode = async (audio: Blob): Promise<number> => {
+			// 1. Decode audio data
+			const arrayBuffer = await audio.arrayBuffer();
+			const audioCtx = new (window.AudioContext || (window as any).webkitAudioContext)();
+			const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
+
+			// 2. Get PCM data from all channels and find max amplitude
+			let max = 0;
+			for (let ch = 0; ch < audioBuffer.numberOfChannels; ch++) {
+				const data = audioBuffer.getChannelData(ch);
+				for (let i = 0; i < data.length; i++) {
+				max = Math.max(max, Math.abs(data[i]));
+				}
+			}
+
+
+			return max
+		};
+
+		const threshold = 0.2; // 0 (silent) to 1 (max)
+		const loudEnough = (await Decode(audio)) > threshold;
+		await new Promise( (res) => { setTimeout(res, 1000) })
+
+		return { label: "Yes", status: 'pass' }
+	}
+	const IsShortEnough: (audio: Blob, duration: number) => Promise<Message> = async (audio: Blob, duration: number) => {
+
+		console.log("Before")
+		await new Promise( (res) => { setTimeout(res, 1000) }) // Load for a while
+		console.log("After")
+
+		if (duration < 4.0)
+			return { label: "Recording was short enough.", status: 'pass' }
+		else
+			return { label: "Our servers can't handle recordings over 4 seconds.", status: 'fail' }
+	}
+
+	const OnRecordingComplete = async (audio: Blob) => {
+		const duration = 1.0
+
+		const checks = [IsLoudEnough, IsShortEnough]
+		var pastChecks: Message[] = []
+		var currentCheck: Message = { label:'', status: 'loading' }
+
+		// Check functions until one fails
+		setChecks([])
+		for (let i = 0; i < checks.length; i++) {
+			const checkFunction = checks[i]
+			currentCheck = { label: 'Loading...', status: 'loading' }
+			setChecks([...pastChecks, currentCheck])
+			currentCheck = await checkFunction(audio, duration)
+			pastChecks = [...pastChecks, currentCheck]
+			setChecks(pastChecks)
+
+			if (currentCheck.status == 'fail' && i+1 > minCheckCount)
+				break
+
+			setMinCheckCount(i)
+		}
+
+		// TODO: Add logic for all checks complete
 	}
 
 	return (
@@ -44,10 +102,13 @@ export default function Captcha() {
 				<div style={{ marginTop: checks.length > 0 ? 12 : 0, width: '100%' }}>
 					{checks.map((check, idx) => (
 						<div key={idx}>
-							{check.passed ? 
+							{check.status == 'pass' ? 
 								RenderMessagebox(check.label, () => {}, 'success', 
 									<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />)
-							: RenderMessagebox(check.label, () => {}, 'error', 
+							: check.status == 'fail' ?
+								RenderMessagebox(check.label, () => {}, 'error', 
+									<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />)
+							: RenderMessagebox(check.label, () => {}, 'warning', 
 									<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />)
 							}
 						</div>
