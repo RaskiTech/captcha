eab73e66ae0a36792579baeee8818b3877a04a15
aarol <aarol@iki.fi>
2025-08-22 18:40:24 +0300

    recorder fixes

--- src/CaptchaRecorder.tsx
+++ src/CaptchaRecorder.tsx
@@ -17,14 +17,15 @@ export function CaptchaRecorder({ onRecordingComplete }: Props) {
     renderRecordedAudio: true,
     continuousWaveform: true,
     continuousWaveformDuration: RECORD_DURATION,
-    scrollingWaveform: false,
   }), []);
+
   const { wavesurfer } = useWavesurfer({
     container: containerRef,
     height: 100,
     waveColor: 'rgba(105, 105, 105, 1)',
     progressColor: 'rgba(45, 45, 45, 1)',
     barWidth: 2,
+    normalize: true,
   })
 
   useEffect(() => {
@@ -36,29 +37,36 @@ export function CaptchaRecorder({ onRecordingComplete }: Props) {
       onRecordingComplete(blob)
     })
     return () => wavesurfer?.destroy()
-    
+
   }, [wavesurfer, record, onRecordingComplete])
 
   return (
     <div className="captcha-box">
       <div className="captcha-content">
-        <div className="captcha-circle" onClick={() => {
-          record.startRecording({}).then(() => {
+        <div className="captcha-circle" onClick={async () => {
+          if (!record.isRecording()) {
+            await record.startRecording({})
             setTimeout(() => {
-              if(record.isRecording()) {
+              if (record.isRecording()) {
                 record.stopRecording()
+                wavesurfer?.empty()
 
               }
             }, RECORD_DURATION * 1000)
-          })
+          } else {
+            record.stopRecording()
+            wavesurfer?.empty()
+          }
         }}>
-          
-            <svg width="32" height="32" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M5 3C5 1.34315 6.34315 0 8 0C9.65685 0 11 1.34315 11 3V7C11 8.65685 9.65685 10 8 10C6.34315 10 5 8.65685 5 7V3Z" fill="#000000" />
-              <path d="M9 13.9291V16H7V13.9291C3.60771 13.4439 1 10.5265 1 7V6H3V7C3 9.76142 5.23858 12 8 12C10.7614 12 13 9.76142 13 7V6H15V7C15 10.5265 12.3923 13.4439 9 13.9291Z" fill="#000000" />
-            </svg>
-         
-        
+
+          <svg style={{
+            fill: record.isRecording() ? 'orange' : 'black',
+          }} width="32" height="32" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 3C5 1.34315 6.34315 0 8 0C9.65685 0 11 1.34315 11 3V7C11 8.65685 9.65685 10 8 10C6.34315 10 5 8.65685 5 7V3Z" />
+            <path d="M9 13.9291V16H7V13.9291C3.60771 13.4439 1 10.5265 1 7V6H3V7C3 9.76142 5.23858 12 8 12C10.7614 12 13 9.76142 13 7V6H15V7C15 10.5265 12.3923 13.4439 9 13.9291Z" />
+          </svg>
+
+
         </div>
         <div className='captcha-wave' ref={containerRef}></div>
       </div>
